827c1dceae69bc63fafda3bbf3f2c51c9aaa2547
Sirisha Challagiri <chsiri469@gmail.com>
2025-10-06 22:13:00 +0530

    Modified security config file

--- payment-backend/payment-backend/src/main/java/com/example/config/SecurityConfig.java
+++ payment-backend/payment-backend/src/main/java/com/example/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,10 +42,27 @@ public class SecurityConfig {
       You’ll see a 403 Forbidden response with a message like:
       */
       .csrf(AbstractHttpConfigurer::disable) //only with this line we can able to login to all pages without authentication
+      .cors(Customizer.withDefaults())
       .authorizeHttpRequests(request -> request //by adding this line to that csrf we can only able to access /hi only remaining all the APIs are not reachable
                             .requestMatchers("/hi").permitAll()
                             .anyRequest().authenticated())
       .httpBasic(Customizer.withDefaults()) // by adding this line to above two lines we are getting basic authentications
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(){
+    UserDetails user1 = User.builder()
+                        .username("admin")
+                        .password("{noop}admin")
+                        .roles("ADMIN")
+                        .build();
+    UserDetails user2 = User.builder()
+                        .username("user")
+                        .password("{noop}user")
+                        .roles("USER")
+                        .build();
+    return new InMemoryUserDetailsManager(user1,user2);
   }
 }
